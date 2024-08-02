803b9e56b9165aa5dbcef362774a9a2fbbd7129f
xreddr <redwardhenion@gmail.com>
2024-08-02 19:42:27 -0400

    added config file

---
+++ config/config.py
@@ -0,0 +1,21 @@
+#Extension groups
+rom_groups = {
+    'NES' : ['.nes'],
+    'SNES' : ['.smc', '.sfc'],
+    'GBC' : ['.gbc', '.gb'],
+    'GBA' : ['.gba'],
+    'MasterSystem' : ['.sms'],
+    'MegaDrive' : ['.bin'],
+    'NeoGeoPocket' : ['.ngp'],
+    "GameGear" : ['.gg'],
+    'PSX' : [],
+    'N64' : ['.n64'],
+    'Dreamcast' : []
+}
+save_groups = {
+    'SNES' : ['.srm'],
+    'GBC' : ['.sav', '.fkfg', '.rtc'],
+    'GBA' : ['.sav'],
+    'PSX' : [],
+    'N64' : []
+}
